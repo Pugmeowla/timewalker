1df0c84e2ab421be70ff6f988f5fcd8ecb38c4d4
Pugmeowla <andretnk49@hotmail.com>
2025-08-27 13:36:23 +1000

    fnsdjkfnksfnskdfns

--- data/timewalker/kubejs_scripts/disable_block_break.js
+++
@@ -1,8 +0,0 @@
-// kubejs/server_scripts/prevent_break.js
-BlockEvents.broken(event => {
-  let player = event.player;
-
-  if (abilityUtil.isEnabled(player, 'timewalker:paradox', 'teleport_command')) {
-    event.cancel();
-  }
-});
